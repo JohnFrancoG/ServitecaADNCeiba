0b291ceac68ff48d455a63abca455f827fd959bc
John Franco <john.franco@ceiba.com.co>
2020-12-02 10:21:39 -0500

    Ajustes

--- microservicio/dominio/src/main/java/com/ceiba/autoMotor/puerto/dao/DaoAutoMotor.java
+++
@@ -1,9 +0,0 @@
-package com.ceiba.autoMotor.puerto.dao;
-
-import com.ceiba.autoMotor.modelo.dto.DtoAutoMotor;
-
-import java.util.List;
-
-public interface DaoAutoMotor {
-    List<DtoAutoMotor> listarAutoMotor();
-}
